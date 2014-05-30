ceb6dabbd08860ba71d5404b9ecd021a916f5503
Iris <414776273@qq.com>
2014-05-30 18:00:34 +0800

    Create the Asp.net mvc 2 project.
    Add ExtJS package.
    Add System Basic Information function.

---
+++ MonitoringSystem/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using System.Net;
+
+namespace MonitoringSystem.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome! There are basic information.";
+            ViewData["HostName"] = getComputerName();
+            ArrayList ipList = getIpAddress();
+            if (ipList.Count == 1)
+            {
+                ViewData["Ipv4"] = ipList[0].ToString();
+                ViewData["Ipv6"] = "None";
+            }
+            else
+            {
+                ViewData["Ipv4"] = ipList[0].ToString();
+                ViewData["Ipv6"] = ipList[1].ToString();
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        //get the IP address
+        public ArrayList getIpAddress()
+        {
+            ArrayList ipList = new ArrayList();
+            IPHostEntry ipe = Dns.GetHostEntry(Dns.GetHostName());
+            for (int i = 0; i < ipe.AddressList.Length; i++)
+            {
+                ipList.Add(ipe.AddressList[i].ToString());
+            }
+            return ipList;
+        }
+
+        //get the computer name
+        public string getComputerName()
+        {
+            return Dns.GetHostName();
+        }
+    }
+}
---
+++ MonitoringSystem/Views/Home/Index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" Culture="auto" UICulture="auto" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Basic Information
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%= Html.Encode(ViewData["Message"]) %></h2>
+    <p>
+        Computer Name: <%= Html.Encode(ViewData["HostName"] )%>
+        <a href="http://asp.net/mvc" title="ASP.NET MVC 网站">http://asp.net/mvc</a>。
+    </p>
+    <p>
+        Ipv4 Address: <%= Html.Encode(ViewData["Ipv4"]) %>
+    </p>
+    <p>
+        Ipv6 Address: <%= Html.Encode(ViewData["Ipv6"]) %>
+    </p>
+</asp:Content>
