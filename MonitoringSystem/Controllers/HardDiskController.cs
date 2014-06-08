8f1a65afc3e9be0635114ed11c7785a4cc3ce52f
IrisTien <tq05s1l@126.com>
2014-06-08 15:12:56 +0800

    Draw the pie chart of hard disk.
    Read data from background.

---
+++ MonitoringSystem/Controllers/HardDiskController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MonitoringSystem.Models;
+
+namespace MonitoringSystem.Controllers
+{
+    public class HardDiskController : Controller
+    {
+        //
+        // GET: /HardDisk/
+
+        public JsonResult GetData()
+        {
+            List<DiskModels> list = new List<DiskModels>();
+            list.Add(new DiskModels() { name = "C disk", data = 10 });
+            list.Add(new DiskModels() { name = "D disk", data = 20 });
+            list.Add(new DiskModels() { name = "E disk", data = 30 });
+            list.Add(new DiskModels() { name = "F disk", data = 40 });
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MonitoringSystem/Models/DiskModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonitoringSystem.Models
+{
+    public class DiskModels
+    {
+        public string name { get; set; }
+        public int data { get; set; }
+    }
+}
